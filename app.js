b5607291715dc9f34228315206da63f7ddc8a456
michichama <thk002@ucsd.edu>
2016-11-10 23:14:12 -0800

    Merging Jacob's branch with main branch.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Require the needed modules and create the app variable
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,13 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
-
-//Set up the app to serve whatever is in the views folder at the url 
+// var gardUp = require('./routes/gardenUpdate'); // new line for garden
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -26,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+// app.get('/useItem', gardUp.updateGarden); //new line for garden
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -44,6 +41,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;

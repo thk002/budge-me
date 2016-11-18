1d995ae29d1b98925cc187b4a818a33a00ce419c
acr012 <acr012@ucsd.edu>
2016-11-17 22:32:07 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,25 @@ var dailyNo = JSON.parse(fs.readFileSync('public/JSON/dailyPenalty.json', 'utf8'
 var weeklyYes = JSON.parse(fs.readFileSync('public/JSON/weeklyRewards.json', 'utf8'));
 var weeklyNo = JSON.parse(fs.readFileSync('public/JSON/weeklyPenalty.json', 'utf8'));
 
-var yesDayValue = random.integer(0, dailyYes.length-1);
-var noDayValue = random.integer(0, dailyNo.length-1);
-var yesWeekValue = random.integer(0, weeklyYes.length-1);
-var noWeekValue = random.integer(0, weeklyNo.length-1);
+var dailyYes2 = JSON.parse(fs.readFileSync('public/JSON/dailyRewards.json', 'utf8'));
+var dailyNo2 = JSON.parse(fs.readFileSync('public/JSON/dailyPenalty.json', 'utf8'));
+var weeklyYes2 = JSON.parse(fs.readFileSync('public/JSON/weeklyRewards.json', 'utf8'));
+var weeklyNo2 = JSON.parse(fs.readFileSync('public/JSON/weeklyPenalty.json', 'utf8'));
+
+
+var yesDayValue = random.integer(0, 6);
+var noDayValue = random.integer(0, 7);
+var yesWeekValue = random.integer(0, 21);
+var noWeekValue = random.integer(0, 5);
 
 
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
+  res.render('login');
+});
+/* GET home page */
+router.get('/home', function(req, res, next) {
   res.render('index');
 });
 /* GET social page. */
@@ -28,10 +38,7 @@ router.get('/social', function(req, res, next) {
 router.get('/budget', function(req, res, next) {
   res.render('budget');
 });
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
+
 /* GET setting page. */
 router.get('/settings', function(req, res, next) {
   res.render('settings');
@@ -72,11 +79,13 @@ router.get('/TBD', function(req, res, next) {
     	// res.send(weeklyItem)
    }
    console.log(dailyItem);
+   // render daily/weekly items based on quest response
    res.render('social', {daily: dailyItem, weekly: weeklyItem});
+   // res.send("Reached Items route")
  
  });
 
- //second page
+ //second page?
 router.get('/social2', function(req, res, next) {
   res.redirect('items2');
 });
@@ -89,31 +98,33 @@ router.get('/social2', function(req, res, next) {
   // dailyItem.push("Test")
   var weeklyItem2=[];
  
-  if (req.query.daily == 'yes') {
+   if (req.query.daily2 == 'yes') {
     //Need to add a random number generator
-    dailyItem.push(dailyYes.dailyRewards[yesDayValue])
+    dailyItem2.push(dailyYes2.dailyRewards[yesDayValue])
     // res.send(dailyItem)
  
    }
-   else if (req.query.daily == 'no') {
+   else {
     //get daily rewards item
-    dailyItem.push(dailyNo.dailyPenalty[noDayValue])
+    dailyItem2.push(dailyNo2.dailyPenalty[noDayValue])
     // res.send(dailyItem)
    }
  
-   if (req.query.weekly == 'yes') {
+   if (req.query.weekly2 == 'yes') {
     //get weekly rewards item
-    weeklyItem.push(weeklyYes.weeklyRewards[yesWeekValue])
+    weeklyItem2.push(weeklyYes2.weeklyRewards[yesWeekValue])
       // res.send(weeklyItem)
  
    }
-   else if (req.query.weekly == 'no') {
+   else {
     //get weekly rewards item
-    weeklyItem.push(weeklyNo.weeklyPenalty[noWeekValue])
+    weeklyItem2.push(weeklyNo2.weeklyPenalty[noWeekValue])
       // res.send(weeklyItem)
    }
+ 
    // render daily/weekly items based on quest response
-   res.render('social2', {daily: dailyItem, weekly: weeklyItem});
+   res.render('social2', {daily2: dailyItem2, weekly2: weeklyItem2});
+   // res.send("Reached Items route")
  
  });
 

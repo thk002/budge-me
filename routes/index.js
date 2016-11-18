05d8cd5734fa6f1d776d0a0aaf0814251b8c0b23
acr012 <acr012@ucsd.edu>
2016-11-17 16:43:45 -0800

    add randomized items

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,19 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 var dailyJSON = require('jsonfile');
+var random = require('random-js')();
 
 var dailyYes = JSON.parse(fs.readFileSync('public/JSON/dailyRewards.json', 'utf8'));
- var dailyNo = JSON.parse(fs.readFileSync('public/JSON/dailyPenalty.json', 'utf8'));
- var weeklyYes = JSON.parse(fs.readFileSync('public/JSON/weeklyRewards.json', 'utf8'));
- var weeklyNo = JSON.parse(fs.readFileSync('public/JSON/weeklyPenalty.json', 'utf8'));
+var dailyNo = JSON.parse(fs.readFileSync('public/JSON/dailyPenalty.json', 'utf8'));
+var weeklyYes = JSON.parse(fs.readFileSync('public/JSON/weeklyRewards.json', 'utf8'));
+var weeklyNo = JSON.parse(fs.readFileSync('public/JSON/weeklyPenalty.json', 'utf8'));
+
+var yesDayValue = random.integer(1, 7);
+var noDayValue = random.integer(1, 8);
+var yesWeekValue = random.integer(1, 22);
+var noWeekValue = random.integer(1, 6);
+
+// console.log(dailyYes.length);
 
 
 /* GET home page. */
@@ -43,25 +51,25 @@ router.get('/TBD', function(req, res, next) {
  
    if (req.query.daily == 'yes') {
    	//Need to add a random number generator
-   	dailyItem.push(dailyYes.dailyRewards[0])
+   	dailyItem.push(dailyYes.dailyRewards[yesDayValue])
    	// res.send(dailyItem)
  
    }
    else if (req.query.daily == 'no') {
    	//get daily rewards item
-   	dailyItem.push(dailyNo.dailyPenalty[0])
+   	dailyItem.push(dailyNo.dailyPenalty[noDayValue])
    	// res.send(dailyItem)
    }
  
    if (req.query.weekly == 'yes') {
    	//get weekly rewards item
-   	weeklyItem.push(weeklyYes.weeklyRewards[0])
+   	weeklyItem.push(weeklyYes.weeklyRewards[yesWeekValue])
     	// res.send(weeklyItem)
  
    }
    else if (req.query.weekly == 'no') {
    	//get weekly rewards item
-  	weeklyItem.push(weeklyNo.weeklyPenalty[0])
+  	weeklyItem.push(weeklyNo.weeklyPenalty[noWeekValue])
     	// res.send(weeklyItem)
    }
    console.log(dailyItem);

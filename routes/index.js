fee6b5cfbc9c12372edfe15ad4d3e1239adb83b1
acr012 <acr012@ucsd.edu>
2016-11-17 22:27:24 -0800

    move link above fold

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,11 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 var dailyJSON = require('jsonfile');
-var random = require('random-js')();
 
 var dailyYes = JSON.parse(fs.readFileSync('public/JSON/dailyRewards.json', 'utf8'));
-var dailyNo = JSON.parse(fs.readFileSync('public/JSON/dailyPenalty.json', 'utf8'));
-var weeklyYes = JSON.parse(fs.readFileSync('public/JSON/weeklyRewards.json', 'utf8'));
-var weeklyNo = JSON.parse(fs.readFileSync('public/JSON/weeklyPenalty.json', 'utf8'));
-
-var yesDayValue = random.integer(0, dailyYes.length-1);
-var noDayValue = random.integer(0, dailyNo.length-1);
-var yesWeekValue = random.integer(0, weeklyYes.length-1);
-var noWeekValue = random.integer(0, weeklyNo.length-1);
-
+ var dailyNo = JSON.parse(fs.readFileSync('public/JSON/dailyPenalty.json', 'utf8'));
+ var weeklyYes = JSON.parse(fs.readFileSync('public/JSON/weeklyRewards.json', 'utf8'));
+ var weeklyNo = JSON.parse(fs.readFileSync('public/JSON/weeklyPenalty.json', 'utf8'));
 
 
 /* GET home page. */
@@ -50,70 +43,31 @@ router.get('/TBD', function(req, res, next) {
  
    if (req.query.daily == 'yes') {
    	//Need to add a random number generator
-   	dailyItem.push(dailyYes.dailyRewards[yesDayValue])
+   	dailyItem.push(dailyYes.dailyRewards[0])
    	// res.send(dailyItem)
  
    }
    else if (req.query.daily == 'no') {
    	//get daily rewards item
-   	dailyItem.push(dailyNo.dailyPenalty[noDayValue])
+   	dailyItem.push(dailyNo.dailyPenalty[0])
    	// res.send(dailyItem)
    }
  
    if (req.query.weekly == 'yes') {
    	//get weekly rewards item
-   	weeklyItem.push(weeklyYes.weeklyRewards[yesWeekValue])
+   	weeklyItem.push(weeklyYes.weeklyRewards[0])
     	// res.send(weeklyItem)
  
    }
    else if (req.query.weekly == 'no') {
    	//get weekly rewards item
-  	weeklyItem.push(weeklyNo.weeklyPenalty[noWeekValue])
+  	weeklyItem.push(weeklyNo.weeklyPenalty[0])
     	// res.send(weeklyItem)
    }
    console.log(dailyItem);
-   res.render('social', {daily: dailyItem, weekly: weeklyItem});
- 
- });
-
- //second page
-router.get('/social2', function(req, res, next) {
-  res.redirect('items2');
-});
-
-/* GET items page. */
- router.get('/items2', function(req, res, next) {
-  
-
-  var dailyItem2=[];
-  // dailyItem.push("Test")
-  var weeklyItem2=[];
- 
-  if (req.query.daily == 'yes') {
-    //Need to add a random number generator
-    dailyItem.push(dailyYes.dailyRewards[yesDayValue])
-    // res.send(dailyItem)
- 
-   }
-   else if (req.query.daily == 'no') {
-    //get daily rewards item
-    dailyItem.push(dailyNo.dailyPenalty[noDayValue])
-    // res.send(dailyItem)
-   }
- 
-   if (req.query.weekly == 'yes') {
-    //get weekly rewards item
-    weeklyItem.push(weeklyYes.weeklyRewards[yesWeekValue])
-      // res.send(weeklyItem)
- 
-   }
-   else if (req.query.weekly == 'no') {
-    //get weekly rewards item
-    weeklyItem.push(weeklyNo.weeklyPenalty[noWeekValue])
-      // res.send(weeklyItem)
-   }
    // render daily/weekly items based on quest response
-   res.render('social2', {daily: dailyItem, weekly: weeklyItem});
+   res.render('social', {daily: dailyItem, weekly: weeklyItem});
+   // res.send("Reached Items route")
  
  });
 
